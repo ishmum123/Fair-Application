135f8a86cde8e197da8a82605de7e9a6e5734ce8
KabirHasan <kabirhasan2514@gmail.com>
2019-01-04 18:35:33 +0600

    [refactor] removed unnecessary navigation items

--- resources/views/layouts/include/footer.blade.php
+++
@@ -1,7 +0,0 @@
-<footer class="pull-right">
-    <div class="copyright-info">
-    <p class="pull-right">Gentelella - Bootstrap Admin Template by <a href="https://colorlib.com">Colorlib</a>  
-    </p>
-    </div>
-    <div class="clearfix"></div>
-</footer>
