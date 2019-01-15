4f9ff6915d87db51b06499c41384db5b09a6e493
KabirHasan <kabirhasan2514@gmail.com>
2019-01-15 10:50:56 +0600

    alhamdulillah

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -61,7 +61,8 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'checkSuper' => \App\Http\Middleware\CheckSuperAdmin::class,
-        'checkAdmin' => \App\Http\Middleware\CheckAdmin::class,
+        'checkAdmin' => \App\Http\Middleware\checkAdmin::class,
+//        'checkAdmin2' => \App\Http\Middleware\CheckSuperAdmin::class,
 
     ];
 
