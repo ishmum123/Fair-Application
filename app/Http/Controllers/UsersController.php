17c90f5817ba8900493e215dc569fbf04cdf173f
KabirHasan <kabirhasan2514@gmail.com>
2019-01-15 09:58:58 +0600

    hello

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -11,7 +11,7 @@ class UsersController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
-        $this->middleware('checkAdmin');
+//        $this->middleware('checkAdmin');
     }
     /**
      * Display a listing of the resource.
