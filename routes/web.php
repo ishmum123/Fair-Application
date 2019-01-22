021a58631dbbf104fddfa3704284d2620c9eaa0d
KabirHasan <kabirhasan2514@gmail.com>
2019-01-23 02:06:48 +0600

    removed all unnecessary file

--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,12 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-//Route::post('/sendmail', function(\Illuminate\Http\Request $request, \Illuminate\Mail\Mailer $mailer){
-//    $mailer
-//    ->to($request->input('mail'))
-//    ->send(new \App\Mail\mymail($request->input('text')));
-//    return redirect('/dashboard');
-//})->name('sendmail');
 
 
 
+Route::get('/', function () {
+    return view('home.welcome');
+})->middleware('guest');
 Auth::routes();
-Route::get('/dashboard', 'DashboardController@dashboard')->name('dashboard');
-Route::resource('infos','InfoController');
 Route::resource('welcomes','WelcomeController');
 Route::resource('admins', 'AdminController');
 Route::resource('users','UserController');
@@ -32,9 +17,6 @@ Route::resource('applications', 'ApplicationController');
 Route::get('applications-approved', 'ApplicationController@approved');
 Route::get('/applications-unapproved', 'ApplicationController@unapproved');
 Route::get('/applications-rejected', 'ApplicationController@rejected');
-//Route::get('/', 'DashboardController@empty');
-Route::get('/', function () {
-    return view('home.welcome');
-})->middleware('guest');
+
 
 
