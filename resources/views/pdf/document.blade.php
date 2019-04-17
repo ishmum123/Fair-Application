7c49837910c41d8af05c67b8bebf43a97afcfae9
Ishmum Jawad Khan <ishmumkhan@gmail.com>
2019-04-17 23:06:40 +0600

    [feature] application summary formatted

--- resources/views/pdf/document.blade.php
+++ resources/views/pdf/document.blade.php
@@ -6,16 +6,23 @@ $applicant = DB::table('users')->where('id',$user_id)->first();
 
 <head>
 <style>
+
 table {
     font-family: arial, sans-serif;
     border-collapse: collapse;
     width: 100%;
+    font-size: 20px;
 }
 
 td, th {
-    border: 1px solid #dddddd;
+    border: 1px solid #00000;
     text-align: left;
     padding: 8px;
+}
+
+p {
+    text-align: center;
+    font-size: 40px;
 }
 
 </style>
@@ -24,61 +31,74 @@ td, th {
 
 <body>
 
+<br/>
+
 <p>বাণিজ্য মেলা আয়োজনের অনুমোদনের জন্য অনলাইন আবেদন ফরম</p>
+
+<br/>
 
 <table>
 <tbody>
 <tr>
-<td width="5%">১.</td>
-<td>মেলা/প্রদর্শনীর নাম : {{ $festival_name }}</td>
+<td width="6%">১.</td>
+<td colspan="6">মেলা/প্রদর্শনীর নাম : {{ $festival_name }}</td>
 </tr>
 <tr>
-<td width="5%">২.</td>
-<td>মেলার ধরণ : {{ $festival_type == 'national' ? 'দেশী' : 'আন্তর্জাতিক' }}</td>
+<td width="6%">২.</td>
+<td colspan="6">মেলার ধরণ : {{ $festival_type == 'national' ? 'দেশী' : 'আন্তর্জাতিক' }}</td>
 </tr>
 <tr>
-<td width="5%">৩.</td>
-<td>মেলা/প্রদর্শনীর মেয়াদকাল : {{ date('F d, Y', strtotime($from) ) }} - {{ date('F d, Y', strtotime($to) ) }}</td>
+<td width="6%">৩.</td>
+<td colspan="6">মেলা/প্রদর্শনীর মেয়াদকাল : {{ date('F d, Y', strtotime($from) ) }} - {{ date('F d, Y', strtotime($to) ) }}</td>
 </tr>
 <tr>
-<td width="5%">৪.</td>
-<td>মেলা/প্রদর্শনীর স্থান (স্থান বরাদ্দপত্র সংযুক্ত করতে হবে) : {{ $festival_place }}</td>
+<td width="6%">৪.</td>
+<td colspan="6">মেলা/প্রদর্শনীর স্থান : {{ $festival_place }}</td>
 </tr>
 <tr>
-<td width="5%">৫.</td>
-<td>আবেদনকারী প্রতিষ্ঠান/সংগঠনের নাম : {{ $applicant->organization_name }}</td>
+<td width="6%">৫.</td>
+<td colspan="6">আবেদনকারী প্রতিষ্ঠান/সংগঠনের নাম : {{ $applicant->organization_name }}</td>
 </tr>
 <tr>
-<td width="5%">৬.</td>
-<td>
-    প্রতিষ্ঠান/সংগঠনের ঠিকানা : {{ $applicant->organization_address }} ****
-    টেলিফোন নম্বর: মোবাইল নম্বর: ইমেইল :
+<td width="6%" rowspan="2">৬.</td>
+<td colspan="6">
+প্রতিষ্ঠান/সংগঠনের ঠিকানা : {{ $applicant->organization_address }} 
+</td>
+<tr>
+<td colspan="2"> টেলিফোন নম্বর : <br/>{{ $applicant->telephone_number }}</td>
+<td colspan="2"> মোবাইল নম্বর : <br/>{{ $applicant->phone_number }}</td>
+<td colspan="2"> ইমেইল : <br/>{{ $applicant->email }}</td>
 </td>
 </tr>
 <tr>
-<td width="5%">৭.</td>
-<td>প্রধান নির্বাহীর নাম/যোগাযোগকারী কর্মকর্তার নাম : </td>
+<td width="6%">৭.</td>
+<td colspan="6">প্রধান নির্বাহীর নাম/যোগাযোগকারী কর্মকর্তার নাম : {{ $applicant->name }}</td>
 </tr>
 <tr>
-<td width="5%">৮.</td>
-<td>কোম্পানি রেজি: নম্বর/ট্রেড লাইসেন্স (হালনাগাদ কপি সংযুক্ত করতে হবে) : {{ $reg_no }}</td>
+<td width="6%">৮.</td>
+<td colspan="6">কোম্পানি রেজি: নম্বর/ট্রেড লাইসেন্স : {{ $reg_no }}</td>
 </tr>
 <tr>
-<td width="5%">৯.</td>
-<td>টিন সার্টিফিকেট ও আয়কর সার্টিফিকেট (হালনাগাদ কপি সংযুক্ত করতে হবে) : {{ $tin_no }}</td>
+<td width="6%">৯.</td>
+<td colspan="6">টিন সার্টিফিকেট ও আয়কর সার্টিফিকেট : {{ $tin_no }}</td>
 </tr>
 <tr>
-<td width="5%">১০.</td>
-<td>ভ্যাট রেজি: নম্বর (হালনাগাদ কপি সংযুক্ত করতে হবে) : {{ $vat_reg_no }}</td>
+<td width="6%">১০.</td>
+<td colspan="6">ভ্যাট রেজি: নম্বর : {{ $vat_reg_no }}</td>
 </tr>
 <tr>
-<td width="5%">১১.</td>
-<td>চালান নম্বর : {{ $chaalan_no }} তারিখ : ব্যাংকের নাম : {{ $bank_name }} শাখার নাম: {{ $branch_name }}</td>
+<td width="6%" rowspan="2">১১.</td>
+<td colspan="2" rowspan="2">চালান নম্বর : <br/>{{ $chaalan_no }} </td> 
+<td colspan="2" rowspan="2"> তারিখ : {{ $date }}</td>
+<td colspan="2"> ব্যাংকের নাম : <br/>{{ $bank_name }} </td>
 </tr>
 <tr>
-<td width="5%">১২.</td>
-<td>চালান ( কোড নং-১১৭০১০০০১২৬৮১) জমাকৃত টাকার পরিমাণ:
-{{ $festival_type == 'national' ? 'স্থানীয় মেলা আয়োজনের ফি ৫০০০/- (পাঁচ হাজার) টাকা।' : 'আন্তর্জাতিক মেলা আয়োজনের ফি ২০০ মার্কিন ডলারের সমপরিমাণ টাকা' }}
+<td colspan="2"> শাখার নাম: <br/>{{ $branch_name }}</td>
+</tr>
+<tr>
+<td width="6%">১২.</td>
+<td colspan="6">চালান ( কোড নং-১১৭০১০০০১২৬৮১) জমাকৃত টাকার পরিমাণ: <br/>
+* {{ $festival_type == 'national' ? 'স্থানীয় মেলা আয়োজনের ফি ৫০০০/- (পাঁচ হাজার) টাকা' : 'আন্তর্জাতিক মেলা আয়োজনের ফি ২০০ মার্কিন ডলারের সমপরিমাণ টাকা' }}
 </td>
 </tr>
 </tbody>
